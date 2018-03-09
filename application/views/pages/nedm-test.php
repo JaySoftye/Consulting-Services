76484e1fcf257f0b7a12cda33a70b6e2063af580
JaySoftye <JaySoftye@Creative-Department-Creative-2-iMac.local>
2018-03-09 13:35:39 -0500

    Repo restored

---
+++ application/views/pages/nedm-test.php
@@ -0,0 +1,281 @@
+<div class="modal fade" id="brochureform" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <div class="modal-content">
+      <div class="modal-body row">
+        <div class="col-md-6">
+          <img src="public/images/ConsultingServices_brochure-image.jpg" class="full" />
+          <p>To <strong>request a copy of our Consulting Services brochure</strong> simply fill out the form and one or our Educational Specialists will reach out to you directly.</p>
+        </div>
+        <div class="col-md-6">
+          <!--[if lte IE 8]>
+          <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+          <![endif]-->
+          <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+          <script>
+          hbspt.forms.create({
+            portalId: '182596',
+            formId: '54a1184e-f9c1-40ad-9983-9d618415e337'
+          });
+          </script>
+        </div>
+      </div>
+    </div>
+</div>
+</div>
+
+<main class="main-content">
+
+  <div class="container-fluid header-container">
+    <div class="top-header">
+      <video poster="public/images/poster-header-image.jpg" playsinline autoplay muted loop>
+        <source src="public/images/consulting-video-background.mp4" type="video/mp4" />
+      </video>
+    </div>
+    <div class="row parallax-first">
+      <div class="col nopadding">
+        <h1 class="hide-small"><img src="public/images/Teq_Consulting_Services_network-text.svg" class="full" alt="Network-Enabled Device Management" /></h1>
+        <div class="col padding-top hide-large">
+          <h1 class="nedm-text">Network-Enabled Device Management</h1>
+          <h5>Understand the impact that adding hundreds of devices, like Chromebooks, iPads, and IFPs (Interactive Flat Panels) will have on your existing network infrastructure.</h5>
+        </div>
+        <div class="card vid-button">
+          <script src="https://fast.wistia.com/embed/medias/fk1nyw3qxr.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><span class="wistia_embed wistia_async_fk1nyw3qxr popover=true popoverAnimateThumbnail=true" style="display:inline-block;height:203px;width:auto;">&nbsp;</span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <section class="gray-bg">
+
+    <div class="container-fluid gray-bg parallax-second">
+      <div class="row padding-bottom">
+        <div class="container">
+          <div class="row padding-top">
+            <div class="col">
+              <h1 class="display-4">Start here.</h1>
+              <p>Teq has completed hundreds of large scale Interactive Flat Panel (IFP) installations and we understand the impact they can have on existing network infrastructure. Below are some sample knowledge based videos on successful Network-Enabled Device Management best practices. For more great videos visit: <a href="https://onlinepd.teq.com/getting-started"> <strong>onlinepd.teq.com/getting-started</strong></a> or <a href="mailto:onlinepd@teq.com"><strong>contact us</strong></a> for more information.</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card nedm-course">
+                <span class="nedm-icon"></span>
+                <div class="card-body">
+                  <small>-</small>
+                  <h3><strong>Adding Interactive Flat Panels to the Classroom</strong></h3>
+                  <p>Should a network enabled interactive flat panel be managed, from a security concern, the same way as any other networked enabled device?</p>
+                  <script src="https://fast.wistia.com/embed/medias/kf88jizmrl.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><span class="wistia_embed wistia_async_kf88jizmrl popover=true popoverAnimateThumbnail=true popoverContent=link" style="display:inline"><a href="#"><button class="button nedm-button">Play Video »</button></a></span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card nedm-course">
+                <span class="nedm-icon"></span>
+                <div class="card-body">
+                  <small>-</small>
+                  <h3><strong>Screen Casting with Your Flat Panel</strong></h3>
+                  <p>What security and general networking concerns should you expect around connecting client-devices to interactive flat panels? </p>
+                  <script src="https://fast.wistia.com/embed/medias/cqgujyvc04.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><span class="wistia_embed wistia_async_cqgujyvc04 popover=true popoverAnimateThumbnail=true popoverContent=link" style="display:inline"><a href="#"><button class="button nedm-button">Play Video »</button></a></span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card nedm-course nedm-bg">
+                <div class="card-body">
+                  <small class="white">-</small>
+                  <h3 class="white"><strong>Continue your learning track today.</strong></h3>
+                  <p class="white">For more great vidoes on Network-Enabled Network Management (NEDM) visit <a href="https://onlinepd.teq.com/getting-started" class="white">onlinepd.teq.com/getting-started</a> and sign up today! <img src="public/images/opd_the_fusion_of_technology_and_learning.svg" alt="Teq Online Professional Development" /></p>
+                  <a href="https://onlinepd.teq.com/getting-started"><button class="button white-fill">More Videos +</button></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid nedm-bg">
+      <div class="container">
+
+        <div class="row padding-top">
+          <div class="col">
+            <h1 class="display-4"><strong>Still need help?</strong><br />Let's get started.</h1>
+          </div>
+        </div>
+        <div class="row padding-top" id="surveyIntro">
+          <div class="col-md-6 survey">
+            <h2><span class="circle-stroke">1</span></h2>
+            <h2><strong>Site Survey</strong></h2>
+            <p>Our site survey is done prior to installing the physical board into the classroom. It is a unique opportunity to ensure your interactive flat panel installation goes as smoothly and hassle free as possible. In addition, we are able to indentify any possible physical problems that may affect the successfull utilization of your IFP.</p>
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <img class="one" src="public/images/nedm-survey-section-intro.svg" alt="" />
+                <img class="two" src="public/images/nedm-survey-section-intro-icons.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md">
+            <img src="public/images/nedm-survey-section-divider2.svg" alt="" />
+          </div>
+        </div>
+        <div class="row" id="sectionOne">
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="animation-one-container">
+                  <img class="one" src="public/images/nedm-survey-section-one-animation-one.svg" alt="" />
+                </div>
+                <div class="animation-two-container">
+                  <img class="two" src="public/images/nedm-survey-section-one-animation-two.svg" alt="" />
+                </div>
+                <img class="cover" src="public/images/nedm-survey-section-one-animation-cover.svg" alt="" />
+                <img src="public/images/nedm-survey-section-one.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 survey">
+            <h2><span class="circle-stroke">2</span></h2>
+            <h2><strong>Network Reporting</strong></h2>
+            <p>Upon installation and site survey completion, Teq is able to perform a two part network survey that will generate a report describing your network infrastructure and performance. By taking 5 minutes to tell us about your network infrastructure, we can provide a more thorough approach to your network devices and their applications. </p>
+            <p><a href="../nedm-survey/"><button class="button nedm-button">Start network survey</button></a></p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md">
+            <img src="public/images/nedm-survey-section-divider1.svg" alt="" />
+          </div>
+        </div>
+        <div class="row" id="sectionTwo">
+          <div class="col-md-6 survey">
+            <h2><span class="circle-stroke">3</span></h2>
+            <h2><strong>Software/Application Reporting</strong></h2>
+            <p>Once the network portion is identified, we then provide an analysis of the software and applications found on your network as well as the impact other existing <em>Smart Devices</em> might have. To do this, we need permission to attach a <strong>ZERO IMPACT</strong> network analyzer on your network. Please make sure you select the appropriate option on the network survey.</p>
+            <img class="col-sm-6" src="public/images/nedm-survey-section-agree-survey-image.svg" alt="" />
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="animation-one-container">
+                  <img class="one" src="public/images/nedm-survey-section-two-animation-two.svg" alt="" />
+                </div>
+                <img src="public/images/nedm-survey-section-two.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md">
+            <img src="public/images/nedm-survey-section-divider2.svg" alt="" />
+          </div>
+        </div>
+        <div class="row" id="sectionThree">
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="animation-one-container">
+                  <img class="one" src="public/images/nedm-survey-section-one-animation-one.svg" alt="" />
+                </div>
+                <div class="animation-two-container">
+                  <img class="two" src="public/images/nedm-survey-section-one-animation-two.svg" alt="" />
+                </div>
+                <img class="cover" src="public/images/nedm-survey-section-three-cover.svg" alt="" />
+                <img src="public/images/nedm-survey-section-three.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 survey">
+            <h2><span class="circle-stroke">4</span></h2>
+            <h2><strong>Managed Network-Device Report</strong></h2>
+            <p>Once the onsite data is collected, we can then provide a full comprehensive report to the successful deployment of all your <em>Smart Devices</em> Using the link below you can download a sample report or feel free to <em>contact us</em> for more information regarding this process or the device details.</p>
+            <p><a href="#"><button class="button sm">Sample Survey</button></a> <a href="mailto:thomasmaher@teq.com"><button class="button sm">Contact Us</button></a></p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md">
+            <img src="public/images/nedm-survey-section-divider1.svg" alt="" />
+          </div>
+        </div>
+        <div class="row" id="sectionFour">
+          <div class="col-md-6 survey">
+            <h2><span class="circle-stroke">5</span></h2>
+            <h2><strong>Prevent Costly Solutions Later On</strong></h2>
+            <p>The idea is not to only provide successfull implementation of <em>Smart Devices</em> across your network but to avoid any expensive investments in the future. Our years of experience has provided us the means to execute the preformance of your devices schools require.</p>
+          </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="animation-one-container">
+                  <img class="one" src="public/images/nedm-survey-section-four-animation-one.svg" alt="" />
+                </div>
+                <div class="animation-two-container">
+                  <img class="two" src="public/images/nedm-survey-section-four-animation-two.svg" alt="" />
+                </div>
+                <img class="cover" src="public/images/nedm-survey-section-four-cover.svg" alt="" />
+                <img src="public/images/nedm-survey-section-four.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row padding-top">
+          <div class="col-md">
+            <h2 class="nedm-text text-center"><strong>Start Today</strong></h2>
+            <p class="text-center"><a href="../nedm-survey/"><button class="button light">Start Your Network Survey Today</button></a></p>
+          </div>
+        </div>
+
+
+        <div class="row padding-top">
+          <div class="col-md">
+
+            <div class="row">
+            	<div id="recipeCarousel" class="carousel slide">
+            		<div class="carousel-inner" role="listbox">
+            			<div class="carousel-item active">
+            				<img class="d-block col-6 img-fluid" src="http://placehold.it/350x150?text=1">
+            			</div>
+            			<div class="carousel-item">
+            				<img class="d-block col-6 img-fluid" src="http://placehold.it/350x150?text=2">
+            			</div>
+            			<div class="carousel-item">
+            				<img class="d-block col-6 img-fluid" src="http://placehold.it/350x150?text=3">
+            			</div>
+            			<div class="carousel-item">
+            				<img class="d-block col-6 img-fluid" src="http://placehold.it/350x150?text=4">
+            			</div>
+            			<div class="carousel-item">
+            				<img class="d-block col-6 img-fluid" src="http://placehold.it/350x150?text=5">
+            			</div>
+            		</div>
+
+            	</div>
+            </div>
+
+<div class="row">
+  <div class="col-md padding">
+    <a class="carousel-control-prev" href="#recipeCarousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#recipeCarousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
+
+  </section>
+
+</main>
---
+++ application/views/pages/si.php
@@ -0,0 +1,178 @@
+<div class="modal fade" id="brochureform" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <div class="modal-content">
+      <div class="modal-body row">
+        <div class="col-md-6">
+          <img src="public/images/ConsultingServices_brochure-image.jpg" class="full" />
+          <p>To <strong>request a copy of our Consulting Services brochure</strong> simply fill out the form and one or our Educational Specialists will reach out to you directly.</p>
+        </div>
+        <div class="col-md-6">
+          <!--[if lte IE 8]>
+          <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+          <![endif]-->
+          <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+          <script>
+          hbspt.forms.create({
+            portalId: '182596',
+            formId: '54a1184e-f9c1-40ad-9983-9d618415e337'
+          });
+          </script>
+        </div>
+      </div>
+    </div>
+</div>
+</div>
+
+<main class="main-content">
+
+  <div class="container-fluid header-container">
+    <div class="top-header">
+      <video poster="" playsinline autoplay muted loop>
+        <source src="public/images/consulting-video-clouds-background.webm" type="video/webm" />
+        <source src="public/images/consulting-video-clouds-background.mp4" type="video/mp4" />
+      </video>
+    </div>
+    <div class="row parallax-first">
+      <div class="col nopadding">
+        <h1 class="hide-small"><img src="public/images/Teq_Consulting_Services_stem-text.svg" class="full" alt="STEM Initiatives" /></h1>
+        <div class="col padding-top hide-large">
+          <h1 class="nedm-text">STEM Initiatives</h1>
+          <h5>While some schools already have clear goals for their STEM programs, others are still exploring their options, and may need guidance in how to design a program that will provide their students with logical STEM sequences that effectively scaffold learning.</h5>
+        </div>
+        <div class="card vid-button">
+          <script src="https://fast.wistia.com/embed/medias/o1btc7i17d.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><span class="wistia_embed wistia_async_o1btc7i17d popover=true popoverAnimateThumbnail=true" style="display:inline-block;height:203px;width:auto;">&nbsp;</span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section>
+
+    <div class="container-fluid brochure stem parallax-second">
+      <div class="container">
+        <div class="row padding-top">
+          <div class="col-md-6">
+            <h3><strong>Teq’s STEM initiatives are curriculum consulting and onsite and online professional development services designed to assist schools in the creation, nurturing, and expansion of K12 STEM programs.</strong></h3>
+            <h5>Whether you are far along in the process of building a comprehensive STEM program, or just starting out, Teq can help. With Teq Online PD, you receive access to wide range of STEM courses for all educational roles.</h5>
+            <p><button type="button" class="button" data-toggle="modal" data-target="#brochureform">Request a brochure</button></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="si-bg">
+
+    <div class="container-fluid gray-bg padding-bottom parallax-third">
+      <div class="row">
+        <div class="container">
+          <div class="row padding-top">
+            <div class="col">
+              <h1 class="display-4">Start here.</h1>
+              <p>Below you will find a few <strong>complimentary sample courses</strong> we have created for all educational roles. Whether you’re far along in the process of building a comprehensive STEM program, or just starting out, we can help. Just about every school and district is unique, because almost everyone is in a different place. For a full list of STEM Courses Visit: <a href="https://onlinepd.teq.com/courses/category/id/44/events/STEM"><strong>opd.teq.com</strong></a> or to gain full access to our course library and learn more about the platform <a href="mailto:onlinepd@teq.com"><strong>contact us</strong></a> for more information.</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card stem-course">
+                <span class="stem-icon"></span>
+                <div class="card-body">
+                  <small>-</small>
+                  <h3><strong>Ultimaker: Optimizing Your 3D Print</strong></h3>
+                  <p>The Cura 2.4 Software by Ultimaker has over 200 settings to adjust your 3D print to fit your needs. Join us as we show you where to download the software and some specific details of how to optimize your 3D print like print setup, materials, and scaling.</p>
+                  <script src="https://fast.wistia.com/embed/medias/0dkknjolmj.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><span class="wistia_embed wistia_async_0dkknjolmj popover=true popoverAnimateThumbnail=true popoverContent=link" style="display:inline"><a href="#"><button class="button stem-button">Play Course »</button></a></span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card stem-course">
+                <span class="stem-icon"></span>
+                <div class="card-body">
+                  <small>-</small>
+                  <h3><strong>Labdisc: Heart Rate</strong></h3>
+                  <p>In this course we'll look at just one of the many applications of a Labdisc in the Biology classroom. You will need a BioChem Labdisc to perform this activity. Our Curriculum Specialists will show you how to conduct an experiment to measure heart rate using the heart rate sensor provided with your Labdisc. They will then show you how to analyze and manipulate the data within the Labdisc software.</p>
+                  <script src="https://fast.wistia.com/embed/medias/bgd9o4dk6u.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><span class="wistia_embed wistia_async_bgd9o4dk6u popover=true popoverAnimateThumbnail=true popoverContent=link" style="display:inline"><a href="#"><button class="button stem-button">Play Course »</button></a></span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card stem-course stem-bg">
+                <div class="card-body">
+                  <small class="white">-</small>
+                  <h3 class="white"><strong>Continue your learning track today.</strong></h3>
+                  <p class="white">For more great courses on STEM visit <a href="https://onlinepd.teq.com/" class="white">opd.teq.com</a> and sign up today! <img src="public/images/opd_the_fusion_of_technology_and_learning.svg" alt="Teq Online Professional Development" /></p>
+                  <a href="https://onlinepd.teq.com/courses/category/id/44/events/STEM"><button class="button white-fill">More Courses +</button></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid si-bg padding-bottom padding-top parallax">
+      <div class="container">
+        <div class="row padding">
+          <div class="col-md-1"></div>
+          <div class="col-md">
+            <h2 class="white text-center"><strong>Not your typical consultant.</strong></h2>
+            <div class="bio-container">
+              <div class="bio-bg">
+                <video poster="public/images/si-Andrew-Grefig-Bio.jpg" playsinline autoplay muted loop>
+                  <source src="public/images/si-Andrew-Grefig-Bio.webm" type="video/webm" />
+                  <source src="public/images/si-Andrew-Grefig-Bio.mp4" type="video/mp4" />
+                </video>
+              </div>
+              <div class="bio-content text-center">
+                <div class="content">
+                <h6>Director of Curriculum and Content</h6>
+                <h2><strong>Andrew Grefig</strong></h2>
+                <p>Andrew started his career by biological field tests as a forensic chemist, before becoming a high school chemistry teacher. He is a veteran employee at Teq, with over 7 years experience as a STEM Curriculum and Professional Development Specialist.</p>
+                <script src="https://fast.wistia.com/embed/medias/5fsh274x34.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><span class="wistia_embed wistia_async_5fsh274x34 popover=true popoverContent=link" style="display:inline"><a href="#"><button class="button stem-button">View Bio</button></a></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-1"></div>
+        </div>
+      </div>
+    </div>
+
+    </section>
+
+    <div class="container-fluid white-bg padding-bottom padding-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-2"></div>
+          <div class="col-md-8">
+            <div class="testimonial-clip row">
+              <div class="col-md">
+                <p class="lead">"Teachers are seeing how Teq is introducing the product… Once the teachers see the how-to, they’re going to just jump right in. And if they have the support with them that'll make them that more efficient. It’s a win win."</p>
+              </div>
+            </div>
+            <div class="testimonial-clip-bottom row">
+              <div class="col-md">
+                <p><strong>- Dr. Timothy Eagen; Superintendent of Kings Park Schools, NY</strong></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-2"></div>
+        </div>
+        <div class="row padding-bottom">
+          <div class="col-sm"></div>
+          <div class="col-sm-5 col-padding">
+            <div class="card vid-button">
+              <a href="http://www.teq.com/testimonials/customer-success-stories/kings-park/">
+                <img src="public/images/Consulting-Services_si_bio_clip_MASK.svg" class="bio-play" />
+              </a>
+              <img src="public/images/consulting-services-si-kings-park-gif.gif" class="modal-content" alt="Kings Park Schools" />
+            </div>
+          </div>
+          <div class="col-sm"></div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+</main>
