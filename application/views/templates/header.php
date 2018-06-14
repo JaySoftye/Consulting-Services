6fdd909a753e1dbaae5e35c8887fa52f354ab5fa
JaySoftye <jonathansoftye@teq.com>
2018-06-14 11:50:16 -0400

    Update to Navigation Links
    accommodate redirect links for HTTP to HTTPS

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Consulting Services</title>
 
-    <link rel="stylesheet" href="http://www.teq.com/consulting-services/public/css/bootstrap.css" />
-    <link rel="stylesheet" href="http://www.teq.com/consulting-services/public/css/main.css" />
+    <link rel="stylesheet" href="../consulting-services/public/css/bootstrap.css" />
+    <link rel="stylesheet" href="../consulting-services/public/css/main.css" />
     <!-- Roboto -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
@@ -15,7 +15,7 @@
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-    <script src="http://www.teq.com/consulting-services/public/js/app.js"></script>
+    <script src="../consulting-services/public/js/app.js"></script>
   </head>
 
 <body>
